9d33a0c181258df3f7fbc9d86ccbfae78d8c43ab
Anish Patel <patelanish223609@gmail.com>
2019-07-01 10:52:28 -0400

    Almost there

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,70 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
 public class Classroom {
+    Student[] students;
+
+
+    public Classroom(Student[] Students){
+        this.students = Students;
+    }
+
+    public Classroom(int maxNumberOfStudents){
+        this.students = new Student[maxNumberOfStudents];
+
+    }
+
+    public Classroom(){
+        this.students = new Student[80];
+    }
+
+    public Student[] getStudents(){
+        return students;
+    }
+
+    public Double getAverageExamScores(){
+        Double sum = 0.0;
+        Integer totalExamScore = 0;
+
+        for (Student student : students){
+            for(Double examScore: student.examScores){
+                sum += examScore;
+                totalExamScore++;
+            }
+        }
+        return sum/totalExamScore;
+    }
+
+    public void addStudent(Student student){
+        ArrayList<Student> studentsList = new ArrayList<>(Arrays.asList(students));
+        studentsList.add(student);
+        students =studentsList.toArray(students);
+    }
+
+    public void removeStudent(String firstName, String lastName){
+        ArrayList<Student> arr = new ArrayList<>();
+     for(int i = 0; i < students.length; i++){
+         arr.add(students[i]);
+     }
+     for (int i= 0; i < students.length; i++){
+         if((students[i].firstName == firstName) && (students[i].lastName == lastName))
+             arr.remove(students[i]);
+     }
+     arr.toArray();
+    }
+
+    public void getStudentsByScore(){
+        ArrayList<Student> arr = new ArrayList<>();
+        for(int i = 0; i < students.length; i++)
+            arr.add(students[i]);
+        Collections.sort(arr, Collections.reverseOrder());
+        arr.toArray();
+    }
 }
+
+// I was getting help but person had to leave so I might have to get another tutor for it.
+//Will for sure get it done though.
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,98 @@
 package io.zipcoder;
 
+import com.sun.deploy.util.ArrayUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+import static com.sun.xml.internal.ws.util.VersionUtil.compare;
+
 public class Student {
+    public String firstName;
+    public String lastName;
+    public ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, Double[] examScore){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        examScores = new ArrayList<>();
+        Double testScores = null;
+        examScores.addAll(Arrays.asList(testScores));
+    }
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+
+        this.firstName = firstName;
+    }
+
+    public String getLastName(){
+
+        return lastName;
+    }
+
+    public void setLastName(String lastName){
+
+        this.lastName = lastName;
+    }
+
+    public String getExamScores(){
+        String scoreResult = "Exam Scores";
+        for (Double score : examScores){
+            scoreResult += "Exam" + (examScores.indexOf(score) +1) + "->" + score.intValue();
+
+        }
+        return scoreResult;
+    }
+
+    public void addExamScore(Double score){
+
+        examScores.add(score);
+    }
+
+    public void setExamScores(int examNumber, double newScore){
+
+        examScores.set(examNumber - 1, newScore);
+    }
+
+    public Integer getAverageExamScore(){
+        Double sum = 0.0;
+        for (Double score : examScores){
+            sum+= score;
+        }
+        return(int)(sum/ examScores.size());
+    }
+
+    public void setExamScores(ArrayList<Double> examScores){
+
+        this.examScores = examScores;
+    }
+
+    public Integer getNumberOfExamsTaken(){
+        return examScores.size();
+    }
+
+    static class studentSorter implements Comparator<Student> {
+
+
+        @Override
+        public int compare(Student o1, Student o2) {
+            int scoreCompare = o2.getAverageExamScore().compareTo(o1.getAverageExamScore());
+            int lastNameCompare = o1.getLastName().compareTo(o2.getFirstName());
+            int firstNameCompare = o1.getFirstName().compareTo(o2.getFirstName());
+
+            if (scoreCompare == 0){
+                return ((lastNameCompare == 0) ? firstNameCompare : lastNameCompare);
+            } else
+            return scoreCompare;
+        }
+    }
+
+
+
 }
